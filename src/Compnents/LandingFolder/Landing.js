2dd6b2bdb82b608828413183fe40d4e9cc560ce1
mj305 <maria.j.beckles@gmail.com>
2020-08-04 21:32:17 -0400

    setup components

---
+++ src/Compnents/LandingFolder/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Landing.css';
+
+const Landing = () => {
+  
+  return (
+    <div>
+      <h1>Landing Component</h1>
+    </div>
+  )
+}
+
+export default Landing;
---
+++ src/Compnents/NavBarFolder/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './NavBar.css';
+
+const NavBar = () => {
+  
+  return (
+    <div>
+      <h1>NavBar Component</h1>
+    </div>
+  )
+}
+
+export default NavBar;
