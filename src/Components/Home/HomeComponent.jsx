94ae3331933527578b9ef6c1575ab5d9e7ea5355
mj305 <maria.j.beckles@gmail.com>
2020-08-10 08:23:53 -0400

    home page cards test

--- src/Components/Home/HomeComponent.jsx
+++ src/Components/Home/HomeComponent.jsx
@@ -7,7 +7,151 @@ const HomeComponent = () => {
   return (
     <>
       <Layout>
-        <h3>Home Component</h3>
+        <div className="columns is-desktop is-mobile" >
+          <div className="card column">
+            <div className="card-image">
+              <figure className="image is-4by3">
+                <img src="https://homemadeforelle.com/wp-content/uploads/2018/09/homemade-scrub-apple-pie-spice.jpg" alt=""/>
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="media">
+
+                <div className="media-content">
+                  <p className="title is-4">Sugar Scrub</p>
+                  <p className="subtitle is-6">Exfoliator</p>
+                </div>
+              </div>
+
+              <div className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+
+          <div className="card column">
+            <div className="card-image">
+              <figure className="image is-4by3">
+                <img src="https://homemadeforelle.com/wp-content/uploads/2018/09/homemade-scrub-apple-pie-spice.jpg" alt=""/>
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="media">
+
+                <div className="media-content">
+                  <p className="title is-4">Sugar Scrub</p>
+                  <p className="subtitle is-6">Exfoliator</p>
+                </div>
+              </div>
+
+              <div className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+
+          <div className="card column">
+            <div className="card-image">
+              <figure className="image is-4by3">
+                <img src="https://homemadeforelle.com/wp-content/uploads/2018/09/homemade-scrub-apple-pie-spice.jpg" alt=""/>
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="media">
+
+                <div className="media-content">
+                  <p className="title is-4">Sugar Scrub</p>
+                  <p className="subtitle is-6">Exfoliator</p>
+                </div>
+              </div>
+
+              <div className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+        </div> 
+
+
+
+        <div className="columns is-desktop" >
+          <div className="card column">
+            <div className="card-image">
+              <figure className="image is-4by3">
+                <img src="https://homemadeforelle.com/wp-content/uploads/2018/09/homemade-scrub-apple-pie-spice.jpg" alt=""/>
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="media">
+
+                <div className="media-content">
+                  <p className="title is-4">Sugar Scrub</p>
+                  <p className="subtitle is-6">Exfoliator</p>
+                </div>
+              </div>
+
+              <div className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+
+          <div className="card column">
+            <div className="card-image">
+              <figure className="image is-4by3">
+                <img src="https://homemadeforelle.com/wp-content/uploads/2018/09/homemade-scrub-apple-pie-spice.jpg" alt=""/>
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="media">
+
+                <div className="media-content">
+                  <p className="title is-4">Sugar Scrub</p>
+                  <p className="subtitle is-6">Exfoliator</p>
+                </div>
+              </div>
+
+              <div className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+
+          <div className="card column">
+            <div className="card-image">
+              <figure className="image is-4by3">
+                <img src="https://homemadeforelle.com/wp-content/uploads/2018/09/homemade-scrub-apple-pie-spice.jpg" alt=""/>
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="media">
+
+                <div className="media-content">
+                  <p className="title is-4">Sugar Scrub</p>
+                  <p className="subtitle is-6">Exfoliator</p>
+                </div>
+              </div>
+
+              <div className="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        
+
+
+
+
+         
+
       </Layout>
     </>
   )
