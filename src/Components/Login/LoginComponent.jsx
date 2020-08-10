3bcef2b4cc337a3c621913ca12502b7a156f94a0
mj305 <maria.j.beckles@gmail.com>
2020-08-09 21:37:03 -0400

    added signup and login forms

--- src/Components/Login/LoginComponent.jsx
+++ src/Components/Login/LoginComponent.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 
+import { useForm } from 'react-hook-form';
+
 import Layout from '../Layout'
 
 const LoginComponent = () => {
 
+
+
   return (
     <>
-      <Layout>
-        <h3>Login Component</h3>
-      </Layout>
+        <Layout> 
+          <div class="field">
+            <label className="label">Email</label>
+              <p class="control has-icons-left has-icons-right">
+               <input class="input" type="email" placeholder="Email"/>
+                <span class="icon is-small is-left">
+                 <i class="fas fa-envelope"></i>
+                </span>
+                <span class="icon is-small is-right">
+                 <i class="fas fa-check"></i>
+                </span>
+              </p>
+          </div>
+          <div class="field">
+            <label className="label">Password</label>
+              <p class="control has-icons-left">
+               <input class="input" type="password" placeholder="Password"/>
+                <span class="icon is-small is-left">
+                 <i class="fas fa-lock"></i>
+                </span>
+              </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <button class="button">Login</button>
+            </p>
+          </div>
+        </Layout>
     </>
   )
 }
--- src/Components/Signup/SignupComponent.jsx
+++ src/Components/Signup/SignupComponent.jsx
@@ -1,15 +1,73 @@
 import React from 'react';
 
+import { useForm } from 'react-hook-form';
+
 import Layout from '../Layout'
 
 const SignupComponent = () => {
 
+  const { register, handleSubmit, errors } = useForm(); // initialise the hook
+  const onSubmit = (data) => {
+      console.log(data);
+      };
+
+
   return (
-    <Layout> 
-      <h3>SIGN UP COMPONENT</h3> 
-    </Layout>
+    <>
+      <Layout>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+              <div classNameName="field" >
+                <label className="label">Name</label>
+                  <div className="control" >
+                    <input name="firstname" className="input" type="text" placeholder="First Name..." ref={register} />
+                    <input name="lastname" className="input" type="text" placeholder="Last Name..." ref={register({ required: true })} />
+                    {errors.lastname && 'Last name is required.'}
+                  </div>
+              </div>
+
+
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control has-icons-left has-icons-right">
+                  <input className="input" type="email" placeholder="Enter email..."/>
+                  <span className="icon is-small is-left">
+                    <i className="fas fa-envelope"></i>
+                  </span>
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle"></i>
+                  </span>
+                </div>
+                {/* <p className="help is-danger">This email is invalid</p> */}
+              </div>
+
+              <div>
+                <label className="label">Password</label>
+
+                  <div className="field">
+                    <div className="control">
+                      <input className="input" type="password" placeholder="Enter Password..."/>
+                    </div>
+                  </div>
+
+                  <div className="field">
+                    <div className="control">
+                      <input className="input" type="password" placeholder="Confirm Password..."/>
+                    </div>
+                  </div>
+              </div>
+
+              <input type="submit" />
+
+            </form>
+
+       </Layout>
+    </>
     
   )
 }
 
 export default SignupComponent
+
+
