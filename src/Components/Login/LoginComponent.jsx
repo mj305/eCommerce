ac3611699a673f899be1771cd091002df26d66da
mj305 <maria.j.beckles@gmail.com>
2020-08-18 14:43:19 -0400

    state

--- src/Components/Login/LoginComponent.jsx
+++ src/Components/Login/LoginComponent.jsx
@@ -1,61 +1,74 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
-import firebase from '../../firebase.config';
+import firebase from "../../firebase.config";
 
-import authContext from '../../Context/AuthContext/authContext'
-import Layout from '../Layout'
+import authContext from "../../Context/AuthContext/authContext";
+import Layout from "../Layout";
 
-import styles from './LoginComponent.module.scss';
+import styles from "./LoginComponent.module.scss";
 
 const LoginComponent = () => {
+  const { logIn } = useContext(authContext);
 
-  const {logIn} = useContext(authContext)
+  const { register, handleSubmit /* , errors */ } = useForm(); // initialise the hook
 
-  const { register, handleSubmit/* , errors */ } = useForm(); // initialise the hook
-  
   const onSubmit = (data) => {
-    logIn(data.email, data.password)
+    logIn(data.email, data.password);
   };
 
   return (
     <>
       <Layout>
-            <h3>LOGIN COMPONENT</h3>
+        <h3>LOGIN COMPONENT</h3>
 
-            <form className={`${styles.loginFormContainer}`} onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className={`${styles.loginFormContainer}`}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control has-icons-left has-icons-right">
+              <input
+                name="email"
+                className="input"
+                type="email"
+                placeholder="Enter email..."
+                ref={register({ required: true })}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-exclamation-triangle"></i>
+              </span>
+            </div>
+          </div>
 
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control has-icons-left has-icons-right">
-                  <input name="email" className="input" type="email" placeholder="Enter email..." ref={register({ required: true })}/>
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-envelope"></i>
-                  </span>
-                  <span className="icon is-small is-right">
-                    <i className="fas fa-exclamation-triangle"></i>
-                  </span>
-                </div>
-              </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <p className="control has-icons-left">
+              <input
+                name="password"
+                className="input"
+                type="password"
+                placeholder="Enter Password"
+                ref={register({ required: true })}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
 
-              <div className="field">
-                  <label className="label">Password</label>
-                    <p className="control has-icons-left">
-                    <input name="password" className="input" type="password" placeholder="Enter Password" ref={register({ required: true })}/>
-                      <span className="icon is-small is-left">
-                      <i className="fas fa-lock"></i>
-                      </span>
-                    </p>
-                </div>
-
-              <button type="submit" className={styles.error}>Submit</button>
-
-            </form>
-
-       </Layout>
+          <button type="submit" className={styles.error}>
+            Submit
+          </button>
+        </form>
+      </Layout>
     </>
-  )
-}
+  );
+};
 
-export default LoginComponent
+export default LoginComponent;
--- src/Components/Signup/SignupComponent.jsx
+++ src/Components/Signup/SignupComponent.jsx
@@ -1,32 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 /* import firebase from '../../firebase.config'; */
 
-import authContext from '../../Context/AuthContext/authContext'
-import Layout from '../Layout'
+import authContext from "../../Context/AuthContext/authContext";
+import Layout from "../Layout";
 
-import styles from './SignupComponent.module.scss';
+import styles from "./SignupComponent.module.scss";
 
 const SignupComponent = () => {
+  const { signUp, loading } = useContext(authContext);
 
-  const {signUp, loading} = useContext(authContext)
-
-  const { register, handleSubmit/* , errors */ } = useForm(); // initialise the hook
+  const { register, handleSubmit /* , errors */ } = useForm(); // initialise the hook
   const onSubmit = (data) => {
-    signUp(data.email, data.password)
+    signUp(data.email, data.password);
   };
-
 
   return (
     <>
       <Layout>
-            <h3>SIGNUP COMPONENT</h3>
+        <h3>SIGNUP COMPONENT</h3>
 
-            <form className={`${styles.signupFormContainer}`} onSubmit={handleSubmit(onSubmit)}>
-
-{/*               <div className="field" >
+        <form
+          className={`${styles.signupFormContainer}`}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          {/*               <div className="field" >
                 <label className="label">Name</label>
                   <div className="control" >
                     <input name="firstname" className="input" type="text" placeholder="First Name..." ref={register} />
@@ -35,50 +35,64 @@ const SignupComponent = () => {
                   </div>
               </div> */}
 
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control has-icons-left has-icons-right">
+              <input
+                name="email"
+                className="input"
+                type="email"
+                placeholder="Enter email..."
+                ref={register({ required: true })}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-exclamation-triangle"></i>
+              </span>
+            </div>
+          </div>
 
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control has-icons-left has-icons-right">
-                  <input name="email" className="input" type="email" placeholder="Enter email..." ref={register({ required: true })}/>
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-envelope"></i>
-                  </span>
-                  <span className="icon is-small is-right">
-                    <i className="fas fa-exclamation-triangle"></i>
-                  </span>
-                </div>
-              </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <p className="control has-icons-left">
+              <input
+                name="password"
+                className="input"
+                type="password"
+                placeholder="Enter Password"
+                ref={register({ required: true })}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
 
-              <div className="field">
-                  <label className="label">Password</label>
-                    <p className="control has-icons-left">
-                    <input name="password" className="input" type="password" placeholder="Enter Password" ref={register({ required: true })}/>
-                      <span className="icon is-small is-left">
-                      <i className="fas fa-lock"></i>
-                      </span>
-                    </p>
-                </div>
+          <div className="field">
+            <label className="label">Confirm Password</label>
+            <p className="control has-icons-left">
+              <input
+                name="confirmpassword"
+                className="input"
+                type="password"
+                placeholder="Confirm Password"
+                ref={register({ required: true })}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
 
-                <div className="field">
-                  <label className="label">Confirm Password</label>
-                    <p className="control has-icons-left">
-                    <input name="confirmpassword" className="input" type="password" placeholder="Confirm Password"ref={register({ required: true })}/>
-                      <span className="icon is-small is-left">
-                      <i className="fas fa-lock"></i>
-                      </span>
-                    </p>
-                </div>
-
-              <button type="submit" className={styles.error}>Submit</button>
-
-            </form>
-
-       </Layout>
+          <button type="submit" className={styles.error}>
+            Submit
+          </button>
+        </form>
+      </Layout>
     </>
-    
-  )
-}
+  );
+};
 
-export default SignupComponent
-
-
+export default SignupComponent;
