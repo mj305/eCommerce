d09feb0e97c8f299cf418fa327ce1b98ee328220
mj305 <maria.j.beckles@gmail.com>
2020-10-14 20:50:32 -0400

    forms styled

--- src/Components/ForgotPassword/ForgotPasswordComponent.jsx
+++ src/Components/ForgotPassword/ForgotPasswordComponent.jsx
@@ -18,13 +18,26 @@ const ForgotPasswordComponent = () => {
 
   return (
     <Layout>
-      <h3>FORGOT PASSWORD COMPONENT</h3>
+      <div className={`${styles.loginHeaderContainer}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#FAE7E7"
+            fill-opacity="1"
+            d="M0,96L80,117.3C160,139,320,181,480,186.7C640,192,800,160,960,176C1120,192,1280,256,1360,288L1440,320L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+          ></path>
+        </svg>
+
+        <h3 className={`${styles.loginHeader}`}>Forgot Password</h3>
+        <p className={`${styles.signupHeaderParragraph}`}>
+          Reset Your Password!
+        </p>
+      </div>
 
       <form
         className={`${styles.forgotFormContainer}`}
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="field">
+        <div className={`field ${styles.formField}`}>
           <label className="label">Email</label>
           <div className="control has-icons-left has-icons-right">
             <input
@@ -43,7 +56,7 @@ const ForgotPasswordComponent = () => {
           </div>
         </div>
 
-        <button type="submit" className={styles.error}>
+        <button type="submit" className={`${styles.submitButton}`}>
           Submit
         </button>
       </form>
--- src/Components/Login/LoginComponent.jsx
+++ src/Components/Login/LoginComponent.jsx
@@ -3,8 +3,6 @@ import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 
-/* import firebase from "../../firebase.config"; */
-
 import authContext from "../../Context/AuthContext/authContext";
 import Layout from "../Layout";
 
@@ -26,13 +24,23 @@ const LoginComponent = () => {
   return (
     <>
       <Layout>
-        <h3>LOGIN COMPONENT</h3>
+        <div className={`${styles.loginHeaderContainer}`}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#FAE7E7"
+              fill-opacity="1"
+              d="M0,96L80,117.3C160,139,320,181,480,186.7C640,192,800,160,960,176C1120,192,1280,256,1360,288L1440,320L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+            ></path>
+          </svg>
+
+          <h3 className={`${styles.loginHeader}`}>Login</h3>
+        </div>
 
         <form
           className={`${styles.loginFormContainer}`}
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div className="field">
+          <div className={`field ${styles.formField}`}>
             <label className="label">Email</label>
             <div className="control has-icons-left has-icons-right">
               <input
@@ -51,7 +59,7 @@ const LoginComponent = () => {
             </div>
           </div>
 
-          <div className="field">
+          <div className={`field ${styles.formField}`}>
             <label className="label">Password</label>
             <p className="control has-icons-left">
               <input
@@ -67,13 +75,15 @@ const LoginComponent = () => {
             </p>
           </div>
 
-          <button type="submit" className={styles.error}>
-            Submit
-          </button>
           <div>
+            <button type="submit" className={`${styles.submitButton}`}>
+              Login
+            </button>
+          </div>
+          <div className={`${styles.formLogin}`}>
             <Link to="/signup">Sign Up</Link>
           </div>
-          <div>
+          <div className={`${styles.formLogin}`}>
             <Link to="/forgotpassword">Forgot Password</Link>
           </div>
         </form>
