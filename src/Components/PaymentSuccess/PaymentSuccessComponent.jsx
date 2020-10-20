efe044098a6ee7705a21504df89d1cf057427d2c
mj305 <maria.j.beckles@gmail.com>
2020-10-20 09:06:07 -0400

    fixed errors

--- src/Components/PaymentSuccess/PaymentSuccessComponent.jsx
+++ src/Components/PaymentSuccess/PaymentSuccessComponent.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Layout from "../Layout";
 
 const PaymentSuccessComponent = () => {
+  useEffect(() => {
+    const clearCart = () => {
+      localStorage.removeItem("cart")
+    }
+    clearCart()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <Layout>
+      
       <h2>Payment Success Component</h2>
     </Layout>
   );
--- src/Components/Signup/SignupComponent.jsx
+++ src/Components/Signup/SignupComponent.jsx
@@ -29,7 +29,7 @@ const SignupComponent = () => {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
               fill="#FAE7E7"
-              fill-opacity="1"
+              fillOpacity="1"
               d="M0,96L80,117.3C160,139,320,181,480,186.7C640,192,800,160,960,176C1120,192,1280,256,1360,288L1440,320L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
             ></path>
           </svg>
